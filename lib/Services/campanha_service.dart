addee27459125c3df9f689bdd84a92c49213e552
Matheus Medeiros Oselame <matheusmedeiros2003@gmail.com>
2025-08-27 21:16:46 -0300

    correção busca de campanhas

--- lib/Services/campanha_service.dart
+++ lib/Services/campanha_service.dart
@@ -6,7 +6,7 @@ import 'package:cfc_vitoria_app/Services/api_base_service.dart';
 class CampanhaService extends ApiServiceBase {
   Future<List<CampanhaRDTO>> obterCampanhas() async {
     try {
-      final response = await get('/Campanhass', autoriza: false);
+      final response = await get('/Campanha', autoriza: false);
       var responseJson = json.decode(response.body);
       return List.generate(responseJson.length, (i) {
         return CampanhaRDTO.fromJson(responseJson[i]);
